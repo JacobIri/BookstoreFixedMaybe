a31f4a8840d1e890e51c600a848000163f641c31
JacobIri <76026530+JacobIri@users.noreply.github.com>
2022-04-21 00:33:38 -0600

    Add project files.

---
+++ Models/EFBuyRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public class EFBuyRepository : IBuyRepository
+    {
+        private BookstoreContext context;
+
+        public EFBuyRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Buy> Buy => context.Buy.Include(x => x.Lines).ThenInclude(x => x.Book);
+
+        public void SaveBuy(Buy buy)
+        {
+            context.AttachRange(buy.Lines.Select(x => x.Book));
+
+            if (buy.BuyId == 0)
+            {
+                context.Buy.Add(buy);
+            }
+
+            context.SaveChanges();
+
+        }
+    }
+}
